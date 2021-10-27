85f162aaa0269af81fdbc1c213c3813578a086f9
rihab <rihab.ed@gmail.com>
2021-10-27 15:46:11 +0100

    first commit

---
+++ src/test/java/com/FirstTest.java
@@ -0,0 +1,18 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+
+	@Test
+	void test1() {
+		//fail("Not yet implemented");
+	}
+	@Test
+	void test2() {
+		//fail("Not yet implemented");
+	}
+
+}
